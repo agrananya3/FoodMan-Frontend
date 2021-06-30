45740a527d4020b2dc59bf509bcdecb678801816
Vedant Kadu <kvedant339@gmail.com>
2021-06-30 16:12:48 +0530

    Updated home page router path.Updated mobile navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import RightColumn from "./components/Layout/RightColumn";
 import Footer from "./components/Layout/Footer/Footer";
 import Navbar from "./components/Layout/Navbar/Navbar";
 import Bag from "./components/UI/BagItem/Bag";
-import { sendCartData,fetchCartData } from './Store/cart-actions';
+import { sendCartData, fetchCartData } from "./Store/cart-actions";
 
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
@@ -26,30 +26,30 @@ const App = () => {
       isInitial = false;
       return;
     }
-    if(cart.changed){
+    if (cart.changed) {
       dispatch(sendCartData(cart));
     }
-     
   }, [cart, dispatch]);
 
   return (
     <React.Fragment>
       <Navbar />
-      <Switch />
-      <Route path="/" exact>
-        <div className="container">
-          <LeftColumn />
-          <MiddleColumn />
-          <RightColumn />
-        </div>
-        <Footer />
-      </Route>
-      <Route path="/bag" exact>
-        <Bag />
-      </Route>
-      <Route path="/orders">
-        <h1>Hello From Account</h1>
-      </Route>
+      <Switch>
+        <Route path="/home" exact>
+          <div className="container">
+            <LeftColumn />
+            <MiddleColumn />
+            <RightColumn />
+          </div>
+          <Footer />
+        </Route>
+        <Route path="/bag">
+          <Bag />
+        </Route>
+        <Route path="/orders">
+          <h1>Hello From Account</h1>
+        </Route>
+      </Switch>
     </React.Fragment>
   );
 };
--- src/components/BagTotal.js
+++ src/components/BagTotal.js
@@ -1,40 +1,53 @@
-import React,{Fragment}from "react";
+import React, { Fragment } from "react";
 import { useSelector } from "react-redux";
-import "./BagTotal.css"
+import { useHistory } from "react-router-dom";
+import "./BagTotal.css";
 
 const BagTotal = (props) => {
-const totalAmount = useSelector((state) => state.cart.totalAmount);
-var grandTotal=totalAmount;
-    if(totalAmount<300)
-        grandTotal+=20;
+  const history = useHistory();
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+  var grandTotal = totalAmount;
+  if (totalAmount < 300) grandTotal += 20;
+
+  const checkOutHandler = () => {
+    history.push("/bag");
+  };
   return (
-      <Fragment>
-        <div className="cart-item-total">
-          <div className="item-total">
+    <Fragment>
+      <div className="cart-item-total">
+        <div className="item-total">
           <span>Item Total</span>
           <span>₹{totalAmount}</span>
-          </div>
-          <div className="Delivery">
-            <span>Delivery</span>
-            {totalAmount>300?(<span className="Delivery-text">Free</span>):(<span>₹20</span>)}
-          </div>
-          {totalAmount<300 && <div className="info">
-            <span >
-              <span >Free delivery above </span>
-              <span >₹300</span>
-            </span>
-          </div>}
         </div>
-        <div>
-            <div className="cart-total">
-            <span>Grand Total</span>
-            <span>₹{grandTotal}</span>
-            </div>
-            <div className="check-btn">
-            <div className="checkout">Proceed To Checkout</div>
-            </div>
-        </div>  
-      </Fragment>
+        <div className="Delivery">
+          <span>Delivery</span>
+          {totalAmount > 300 ? (
+            <span className="Delivery-text">Free</span>
+          ) : (
+            <span>₹20</span>
+          )}
+        </div>
+        {totalAmount < 300 && (
+          <div className="info">
+            <span>
+              <span>Free delivery above </span>
+              <span>₹300</span>
+            </span>
+          </div>
+        )}
+      </div>
+      <div>
+        <div className="cart-total">
+          <span>Grand Total</span>
+          <span>₹{grandTotal}</span>
+        </div>
+        <div className="check-btn">
+          <div className="checkout" onClick={checkOutHandler}>
+            Proceed To Checkout
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
